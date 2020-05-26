a186a2683f0ad58332f97ba210fb86189073c57d
Fernando Ferreira Rocha <fernandofr510@gmail.com>
2020-05-26 06:40:01 -0700

    add celebrate in routes

--- src/modules/categorys/infra/http/routes/categorys.routes.ts
+++ src/modules/categorys/infra/http/routes/categorys.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import CategorysController from '../controller/CategorysController';
 
 const categorysRouter = Router();
 const categorysController = new CategorysController();
 
 categorysRouter.get('/', categorysController.index);
-categorysRouter.post('/', categorysController.create);
+categorysRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      title: Joi.string().required(),
+    },
+  }),
+  categorysController.create,
+);
 
 export default categorysRouter;
--- src/modules/transactions/infra/http/routes/transactions.routes.ts
+++ src/modules/transactions/infra/http/routes/transactions.routes.ts
@@ -1,11 +1,29 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import TransactionsController from '../controllers/TransactionsController';
 
 const transactionsRouter = Router();
 const transactionsController = new TransactionsController();
 
 transactionsRouter.get('/', transactionsController.index);
-transactionsRouter.post('/', transactionsController.create);
-transactionsRouter.delete('/:id', transactionsController.delete);
+transactionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      title: Joi.string().required(),
+      value: Joi.number().required(),
+      type: Joi.string().valid('entrada', 'saida').required(),
+      category: Joi.string().required(),
+    },
+  }),
+  transactionsController.create,
+);
+transactionsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+  }),
+  transactionsController.delete,
+);
 
 export default transactionsRouter;
